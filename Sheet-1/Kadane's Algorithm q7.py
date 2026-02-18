b1d4e5c548035291d78c8c2f9bdc93a64e90691c
vanshnijhawan08 <vanshnijhawan08@gmail.com>
2026-02-18 14:51:33 +0530

    Kadane's Algorithm q7.py

---
+++ Sheet-1/Kadane's Algorithm q7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubarraySum(self, arr):
+        current_sum = arr[0]
+        max_sum = arr[0]
+        
+        for i in range(1, len(arr)):
+            if current_sum + arr[i] > arr[i]:
+                current_sum = current_sum + arr[i]
+            else:
+                current_sum = arr[i]
+            
+            if current_sum > max_sum:
+                max_sum = current_sum
+        
+        return max_sum
